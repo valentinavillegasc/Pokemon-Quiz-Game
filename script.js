29b314b2960dd69dda64ef0cf15ea763832455a6
Valentina Villegas <valentinavillegasc@hotmail.com>
2024-05-27 12:47:51 -0500

    fix

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const totalCount = document.getElementById("totalCount");
 const mainContainer = document.getElementById("container");
 const LoadingContainer = document.getElementById("loadingContainer");
 
+let usedPokemonIds = [];
+
 async function fetchPokemonById(id) {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
   const data = await response.json();
   return data;
 }
-
-let usedPokemonIds = [];
 
 //Function to load questio with options
 async function loadQuestionWithOptions() {
@@ -35,10 +35,9 @@ async function loadQuestionWithOptions() {
     }
     optionsIds.push(randomPokemonId);
     const randomPokemon = await fetchPokemonById(randomPokemonId);
-    const randomOption = randomPokemon.nameoptions.push(randomOption);
+    const randomOption = randomPokemon.name;
+    options.push(randomOption);
   }
-  console.log(options);
-  console.log(optionsIds);
 }
 
 loadQuestionWithOptions();
